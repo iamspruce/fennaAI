4d4b8eb7c3b814fdf2749ba07c37ddbed0f9f94b
iamspruce <iamspruce8@gmail.com>
2025-09-02 08:55:21 +0100

    changed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   // Replace with your actual domain
-  site: "https://www.spruceemmanuel.com",
+  site: "https://www.fennaai.com",
 
   integrations: [icon()],
 });
